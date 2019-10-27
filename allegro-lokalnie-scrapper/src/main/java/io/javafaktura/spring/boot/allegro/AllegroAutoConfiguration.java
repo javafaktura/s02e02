ee10ce331db42540b82eb4c27b907bc909c91b54
Adam Krol <adam.krol@idemia.com>
2019-10-27 09:00:41 +0100

    Added starter for Allegro.

---
+++ allegro-lokalnie-scrapper/src/main/java/io/javafaktura/spring/boot/allegro/AllegroAutoConfiguration.java
@@ -0,0 +1,12 @@
+package io.javafaktura.spring.boot.allegro;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Adam Król 2019-10-27
+ */
+@Configuration
+@ComponentScan("io.javafaktura.spring.boot.allegro")
+public class AllegroAutoConfiguration {
+}
--- scrapper/src/main/java/io/javafaktura/spring/boot/scrapper/app/ScrapperApplication.java
+++ scrapper/src/main/java/io/javafaktura/spring/boot/scrapper/app/ScrapperApplication.java
@@ -2,18 +2,16 @@ package io.javafaktura.spring.boot.scrapper.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author Adam Król 2019-10-19
  */
 @SpringBootApplication
-@ComponentScan("io.javafaktura.spring.boot") //Moved from ScrapperConfig
 @PropertySource("classpath:/application.properties")
 public class ScrapperApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ScrapperApplication.class, args); //This can return context
+        SpringApplication.run(ScrapperApplication.class, args);
     }
 }
